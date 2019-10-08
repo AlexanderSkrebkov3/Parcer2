b2116e6b9f540ed2d8acfcf0417dd0aa124938bd
AlexanderSkrebkov3 <42012166+AlexanderSkrebkov3@users.noreply.github.com>
2019-10-08 18:10:59 +0300

    Update Parcer.cpp

--- Parcer.cpp
+++ Parcer.cpp
@@ -8,124 +8,124 @@
 using namespace std;
 class Json {
 	any data; // unordered_map<string, any> ob; или vector<any> ob;
+	unordered_map<string, any> Ob1;
+	vector<any> Ob2;
 public:
 	// Конструктор из строки, содержащей Json-данные.
 	Json(const std::string& s)
 	{
 		bool flag, flag2;
-		unordered_map<string, any> Ob1;
-		vector<any> Ob2;
 		if (s[0] == '{') {
 			flag = true; // Объект не массив
 		}
 		if (s[0] == '[') {
-		flag = false; // Массив
+			flag = false; // Массив
 		}
 		if (!(s[0] == '{' || s[0] == '[')) throw exception("Неверный формат строки");
 		void *pointer = nullptr; // Для задания null
 		string key = ""; // Ключ
 		double x; int a; // Для чтения вещественных и целых значений
 			// Ищем ключ - строку
-			
-			int k1=0, k2; // Индексы для перемещения по строке
-			while(1) // Цикл поиска ключа и значения
+
+		int k1 = 0, k2; // Индексы для перемещения по строке
+		while (1) // Цикл поиска ключа и значения
+		{
+			if (flag) // Чиатем ключ для объекта
 			{
-				if (flag) // Чиатем ключ для объекта
+				k1 = (int)s.find("\"", k1); // начало строки "
+				if (k1 < 0) throw exception("Неверный формат строки");
+				k2 = (int)s.find("\"", k1 + 1); // конец строки "
+				if (k2 < 0) throw exception("Неверный формат строки");
+				key = s.substr(k1 + 1, k2 - k1 - 1); // Извлекаем ключ
+				k1 = (int)s.find(":", k2 + 1); // Ищем : после ключа
+				if (k1 < 0) throw exception("Неверный формат строки");
+
+			}
+			k1++; // Символ за : или за [
+			string sOb;
+			// Цикл чтения значения
+			while (1) // Перебираем символы разделители пропускаем
+			{
+				// k1 - текущий индекс строки с неготначинаем чтение
+				if (k1 >= s.size()) throw exception("Неверный формат строки");
+				if (s[k1] == ',') if (flag) break; else k1++;
+				if (s[k1] == '}') if (flag) break; else throw exception("Неверный формат строки");
+				if (s[k1] == ']') if (!flag) break; else throw exception("Неверный формат строки");
+				if (s[k1] != ' ' && s[k1] != '\n' && s[k1] != '\r' && s[k1] != '\t') // Ищем не разделитель
 				{
-					k1 = (int)s.find("\"", k1); // начало строки "
-					if (k1 < 0) throw exception("Неверный формат строки");
-					k2 = (int)s.find("\"", k1 + 1); // конец строки "
-					if (k2 < 0) throw exception("Неверный формат строки");
-					key = s.substr(k1 + 1, k2 - k1 - 1); // Извлекаем ключ
-					k1 = (int)s.find(":", k2 + 1); // Ищем : после ключа
-					if (k1 < 0) throw exception("Неверный формат строки");
-					
-				}
-				k1++; // Символ за : или за [
-				string sOb;
-				// Цикл чтения значения
-				while (1) // Перебираем символы разделители пропускаем
-				{
-					// k1 - текущий индекс строки с неготначинаем чтение
-					if (k1>=s.size()) throw exception("Неверный формат строки");
-					if (s[k1] == ',') if (flag) break; else k1++;
-					if (s[k1] == '}') if (flag) break; else throw exception("Неверный формат строки");
-					if (s[k1] == ']') if (!flag) break; else throw exception("Неверный формат строки");
-					if (s[k1] != ' ' && s[k1] != '\n' && s[k1] != '\r' && s[k1] != '\t') // Ищем не разделитель
+					switch (s[k1]) // Определяем тип значения
 					{
-						switch (s[k1]) // Определяем тип значения
+					case '{': // Это объект
+						k2 = (int)s.find("}", k1 + 1); // конец объекта
+						if (k2 < 0) throw exception("Неверный формат строки");
+						sOb = s.substr(k1, k2 - k1 + 1); // Строка задающая объект
+					//	any_cast<unordered_map<string, any> *>(data)->insert({ key, any_cast<any>(Json(sOb)) });
+						if (flag) Ob1.insert({ key, Json(sOb) }); // Рекурсивный вызов конструктора для вложенного объекта
+						else Ob2.push_back(Json(sOb));// Рекурсивный вызов конструктора для вложенного объекта
+						k2++; // Переход к след символу за }
+						break;
+					case '[': // Значение массив
+					{
+						k2 = (int)s.find("]", k1 + 1); // конец массива
+						if (k2 < 0) throw exception("Неверный формат строки");
+						sOb = s.substr(k1, k2 - k1 + 1); // Строка задающая объект
+						if (flag) Ob1.insert({ key, Json(sOb) }); // Рекурсивный вызов конструктора для вложенного объекта
+						else Ob2.push_back(Json(sOb));// Рекурсивный вызов конструктора для вложенного объекта
+						k2++; // Переход к след символу за ]
+						break;
+					}
+					case '\"': // Значение строка
+						k2 = (int)s.find("\"", k1 + 1); // конец строки
+						if (k2 < 0) throw exception("Неверный формат строки");
+						sOb = s.substr(k1 + 1, k2 - k1 - 1); // Значение - простая строка
+						if (flag) Ob1.insert({ key, sOb });
+						else Ob2.push_back(sOb);
+						k2++; // Переход к след символу за " (концом строки)
+						break;
+					case 'f':   case 't':  // значение true или false
+						if (s[k1] == 'f') flag2 = false;
+						else flag2 = true;
+						k2 = k1 + 1;
+						while (isalpha(s[k2])) k2++; // Пропускаем символы - буквы
+						if (flag) Ob1.insert({ key, flag2 });
+						else Ob2.push_back(flag2);
+						break;
+					case 'n':   // null
+						k2 = k1 + 1;
+						while (isalpha(s[k2])) k2++; // Пропускаем символы - буквы
+						if (flag) Ob1.insert({ key, pointer });
+						else Ob2.push_back(pointer);
+						break;
+					default:
+						// Читаем значение как число
+						stringstream ss(s.substr(k1));
+						ss >> x;
+						if (ss.fail()) // Ошибка чтения
+							throw exception("Неверный формат строки");
+						// Проверяем целое число или вещественное
+						a = x;
+						if (flag)
 						{
-						case '{': // Это объект
-							k2 = (int)s.find("}", k1 + 1); // конец объекта
-							if (k2 < 0) throw exception("Неверный формат строки");
-							sOb = s.substr(k1, k2 - k1 + 1); // Строка задающая объект
-						//	any_cast<unordered_map<string, any> *>(data)->insert({ key, any_cast<any>(Json(sOb)) });
-							if (flag) Ob1.insert({ key, Json(sOb) }); // Рекурсивный вызов конструктора для вложенного объекта
-							else Ob2.push_back(Json(sOb));// Рекурсивный вызов конструктора для вложенного объекта
-							k2++; // Переход к след символу за }
-							break;
-						case '[': // Значение массив
-						{
-							k2 = (int)s.find("]", k1 + 1); // конец массива
-							if (k2 < 0) throw exception("Неверный формат строки");
-							sOb = s.substr(k1, k2 - k1 + 1); // Строка задающая объект
-							if (flag) Ob1.insert({ key, Json(sOb) }); // Рекурсивный вызов конструктора для вложенного объекта
-							else Ob2.push_back(Json(sOb));// Рекурсивный вызов конструктора для вложенного объекта
-							k2++; // Переход к след символу за ]
-							break;
+							if (a == x) Ob1.insert({ key, a }); // Целое
+							else Ob1.insert({ key, x }); // Вещественное
 						}
-						case '\"': // Значение строка
-							k2 = (int)s.find("\"", k1 + 1); // конец строки
-							if (k2 < 0) throw exception("Неверный формат строки");
-							sOb = s.substr(k1+1, k2 - k1 - 1); // Значение - простая строка
-							if (flag) Ob1.insert({ key, sOb });
-							else Ob2.push_back(sOb);
-						    k2++; // Переход к след символу за " (концом строки)
-							break;
-						case 'f':   case 't':  // значение true или false
-							if (s[k1]=='f') flag2 = false;
-							else flag2 = true;
-							k2 = k1 + 1; 
-							while (isalpha(s[k2])) k2++; // Пропускаем символы - буквы
-							if (flag) Ob1.insert({ key, flag2 });
-							else Ob2.push_back(flag2);
-							break;
-						case 'n':   // null
-							k2 = k1 + 1;
-							while (isalpha(s[k2])) k2++; // Пропускаем символы - буквы
-							if (flag) Ob1.insert({ key, pointer });
-							else Ob2.push_back(pointer);
-							break;
-						default:
-							// Читаем значение как число
-							stringstream ss(s.substr(k1));
-							ss >> x;
-							if (ss.fail()) // Ошибка чтения
-								throw exception("Неверный формат строки");
-							// Проверяем целое число или вещественное
-							a = x;
-							if (flag)
-							{
-								if (a == x) Ob1.insert({ key, a }); // Целое
-								else Ob1.insert({ key, x }); // Вещественное
-							}
-							else
-								if (a == x) Ob2.push_back(a); // Целое
-								else Ob2.push_back(x); // Вещественное
+						else
+							if (a == x) Ob2.push_back(a); // Целое
+							else Ob2.push_back(x); // Вещественное
 
-							k2 = k1 + 1;
-							while (s[k2] >= '0' && s[k2] <= '9' || s[k2] == '.') k2++; // Пропускаем части числа цифры и .
+						k2 = k1 + 1;
+						while (s[k2] >= '0' && s[k2] <= '9' || s[k2] == '.') k2++; // Пропускаем части числа цифры и .
 
-						} // switch (s[k])
-						k1 = k2;
-						
-					} // if (s[k] != ' ' && s[k] != '\n' && s[k] != '\r'); // Ищем не разделитель
-					else k1++; // Символы разделители пропускаем
-					if (s[k1] == ']') break;
-				} // while(1)
-			
-				if (s[k1] == ']' || s[k1] == '}') break;
+					} // switch (s[k])
+					k1 = k2;
+
+				} // if (s[k] != ' ' && s[k] != '\n' && s[k] != '\r'); // Ищем не разделитель
+				else k1++; // Символы разделители пропускаем
+				if (s[k1] == ']') break;
 			} // while(1)
+
+			if (s[k1] == ']' || s[k1] == '}') break;
+		} // while(1)
 
 		if (flag) data = Ob1;
 		else data = Ob2;
@@ -134,15 +134,15 @@ public:
 	void print()
 	{
 		if (is_object()) // Печать объекта
-		{   
+		{
 			cout << endl << "{";
 			int i = 0;
 			for (auto pos : any_cast<unordered_map<string, any>>(data))
 			{
 				if (i > 0) cout << ", ";
-				cout << endl << pos.first<<": "; // Ключ
+				cout << endl << pos.first << ": "; // Ключ
 				// Печать значений
-			    if (pos.second.type() == typeid(string)) cout << any_cast<string>(pos.second);
+				if (pos.second.type() == typeid(string)) cout << any_cast<string>(pos.second);
 				if (pos.second.type() == typeid(int)) cout << any_cast<int>(pos.second);
 				if (pos.second.type() == typeid(double)) cout << any_cast<double>(pos.second);
 				if (pos.second.type() == typeid(bool)) cout << any_cast<bool>(pos.second);
@@ -168,11 +168,11 @@ public:
 				if (pos.type() == typeid(Json)) any_cast<Json>(pos).print();
 				i++;
 			}
-			cout <<  "]";
+			cout << "]";
 		}
 	}
 
-	
+
 	// Метод возвращает true, если данный экземпляр содержит в себе JSON-массив. Иначе false.
 	bool is_array() const
 	{
@@ -191,8 +191,11 @@ public:
 	// Если экземпляр является JSON-массивом, генерируется исключение.
 	std::any& operator[](const std::string& key)
 	{
-		if (data.type() == typeid(vector<any>)) throw exception("Неверный тип");
-		return any_cast<unordered_map<string, any>>(data)[key];
+
+		if (data.type() == typeid(vector<any>)) {
+			throw exception("Неверный тип");
+		}
+		return 	Ob1[key];
 	}
 
 	// Метод возвращает значение по индексу index, если экземпляр является JSON-массивом.
@@ -200,8 +203,8 @@ public:
 	// Если экземпляр является JSON-объектом, генерируется исключение.
 	std::any& operator[](int index)
 	{
-		if (typeid(data) != typeid(vector<any>)) throw exception("Неверный тип");
-		return any_cast<vector<any>>(data)[index];
+		if (data.type() != typeid(vector<any>)) throw exception("Неверный тип");
+		return Ob2[index];
 	}
 
 
@@ -234,6 +237,7 @@ int main()
 	// Строки для тестирования
 	try
 	{
+		setlocale(LC_ALL, "rus");
 		string teststr = "{\
 		\"lastname\" : \"Ivanov\",\
 			\"firstname\" : \"Ivan\",\
@@ -247,17 +251,41 @@ int main()
 						\"street\" : \"Vozdvijenka\"\
 				}\
 	}";
-		string str1 = "[\"Hello1\", 1.5, \"Hello3\", 2]";
-		string str2 = "{\"Hello\": true, \"World\": false}";
+		//string str1 = "[\"Hello1\", 1.5, \"Hello3\", 2]";
+		//string str2 = "{\"Hello\": true, \"World\": false}";
 
-		//	Json Js1(teststr);
-		Json Js1 = Json::parseFile("Proba.txt");
+		//	Json Js1(teststr); // Чтение из строки
+		Json Js1 = Json::parseFile("Proba.txt"); // Чтение из файла
 		Js1.print();
-		cout << endl << any_cast<string>(Js1["lastname"]);
+		// Обращение в элементам Объекта
+		cout << endl << "Обращение к карте по ключу lastname : " << any_cast<string&>(Js1["lastname"]);
+		cout << endl << "Обращение к карте по ключу islegal: " << any_cast<bool>(Js1["islegal"]);
+		cout << endl << "Обращение к карте по ключу age: " << std::any_cast<int>(Js1["age"]);
+
+		auto marks = std::any_cast<Json>(Js1["marks"]); // Получаем массив оценок
+		// Обращение в элементам массива
+		for (int i = 0; i < 6; i++)
+		{
+			cout << endl << "Обращение к массиву по индексу marks: " << std::any_cast<int>(marks[i]);
+		}
+		
+		//cout << endl << "Обращение к массиву по индексу: " << std::any_cast<int>(marks[1]);
+
+
+		auto address = std::any_cast<Json>(Js1["address"]); // Получаем вложенный объект адрес
+		cout << endl << "Обращение к карте по ключу city: " << any_cast<string>(address["city"]);
+		cout << endl << "Обращение к карте по ключу street: " << std::any_cast<string>(address["street"]);
+
+
 	}
 	catch (exception e)
 	{
 		cout << endl << "Exception: " << e.what();
 	}
+	catch (...)
+	{
+		cout << endl << "Exception!!!!!!!!!!!";
+	}
+	system("pause");
 	return 0;
 }
